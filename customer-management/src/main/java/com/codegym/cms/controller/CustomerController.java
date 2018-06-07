534be470e494a89a8b52039cad7a45edc469819c
tungnk_2711 <tungdemo@gmail.com>
2018-06-07 17:29:07 +0700

    init project

---
+++ customer-management/src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -0,0 +1,96 @@
+package com.codegym.cms.controller;
+
+import java.util.List;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    //-------------------Retrieve All Customers--------------------------------------------------------
+
+    @RequestMapping(value = "/customers/", method = RequestMethod.GET)
+    public ResponseEntity<List<Customer>> listAllCustomers() {
+        List<Customer> customers = customerService.findAll();
+        if (customers.isEmpty()) {
+            return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<Customer>>(customers, HttpStatus.OK);
+    }
+
+    //-------------------Retrieve Single Customer--------------------------------------------------------
+
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
+        System.out.println("Fetching Customer with id " + id);
+        Customer customer = customerService.findById(id);
+        if (customer == null) {
+            System.out.println("Customer with id " + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+    }
+
+    //-------------------Create a Customer--------------------------------------------------------
+
+    @RequestMapping(value = "/customers/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer, UriComponentsBuilder ucBuilder) {
+        System.out.println("Creating Customer " + customer.getLastName());
+        customerService.save(customer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/customers/{id}").buildAndExpand(customer.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    //------------------- Update a Customer --------------------------------------------------------
+
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer) {
+        System.out.println("Updating Customer " + id);
+
+        Customer currentCustomer = customerService.findById(id);
+
+        if (currentCustomer == null) {
+            System.out.println("Customer with id " + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+
+        currentCustomer.setFirstName(customer.getFirstName());
+        currentCustomer.setLastName(customer.getLastName());
+        currentCustomer.setId(customer.getId());
+
+        customerService.save(currentCustomer);
+        return new ResponseEntity<Customer>(currentCustomer, HttpStatus.OK);
+    }
+
+    //------------------- Delete a Customer --------------------------------------------------------
+
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting Customer with id " + id);
+
+        Customer customer = customerService.findById(id);
+        if (customer == null) {
+            System.out.println("Unable to delete. Customer with id " + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+
+        customerService.remove(id);
+        return new ResponseEntity<Customer>(HttpStatus.NO_CONTENT);
+    }
+}
